8a939b3bfb9c9f584f3e294c4e44473d1f659fd7
Anthony Lima <anthcostalima@gmail.com>
2024-08-13 16:41:11 -0300

    first commit

---
+++ src/test/java/com/example/atividade_marcio/AtividadeMarcioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividade_marcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeMarcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
