e938740992347026f902d459b9d8be724d1a6dff
Anthony Lima <anthcostalima@gmail.com>
2024-10-03 22:45:26 -0300

    atividade

--- src/main/java/com/example/atividade_marcio/controller/CurriculoController.java
+++ src/main/java/com/example/atividade_marcio/controller/CurriculoController.java
@@ -1,9 +1,10 @@
 package com.example.atividade_marcio.controller;
-
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.atividade_marcio.models.Curriculo;
-
+import com.example.atividade_marcio.services.CurriculoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,13 +15,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/curriculo")
 @RestController
 public class CurriculoController {
+    @Autowired
+    CurriculoService service;
+
+
     @PostMapping()
-    public String insert(@RequestBody Curriculo entity) {
-        return "TESTE DE ADICIONAR ALGO";
+    public int insert(@RequestBody Curriculo entity) {
+        try{
+            service.insertACurriculum(entity);
+            return 1;
+        }
+        catch(Exception e){
+            return 0;
+        }
     }
     @GetMapping()
-    public String select(@RequestBody Curriculo entity) {
-        return "TESTE DE PEGAR ALGO";
+    public List<Curriculo> select() {
+        
+            return service.getAll();
+        
+    }
+    
+    @DeleteMapping()
+    public boolean delete(@RequestParam int id) {
+        try{
+            service.deleteCurriculum(id);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
     }
 
     
